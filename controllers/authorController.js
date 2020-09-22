e900bbaef0c753b7e5ab7f89bc7b02bce7f9a802
reactNoobie <reactnoobie@gmail.com>
2020-09-22 22:56:44 +0600

    Added routes and controllers
    catalog.js defines routes and maps to appropriate controller methods
    Defined controller methods for author, book, bookinstance and genre

---
+++ controllers/authorController.js
@@ -0,0 +1,49 @@
+var Author = require('../models/author');
+
+// Display list of all Authors
+exports.author_list = function (req, res, next) {
+	Author.find()
+		.sort([['family_name', 'ascending']])
+		.exec(function (err, author_list) {
+			if (err) {
+				return next(err);
+			}
+			res.render('author_list', { title: 'Author list', author_list: author_list });
+		});
+};
+
+// Display detail page for specific Author
+exports.author_detail = function (req, res) {
+	res.send(`TODO: Author detail: ${req.params.id}`);
+};
+
+// Display Author create form on GET
+exports.author_create_get = function (req, res) {
+	res.send('TODO: Author create GET');
+};
+
+// Handle Author create on POST
+exports.author_create_post = function (req, res) {
+	res.send('TODO: Author create POST');
+};
+
+// Display Author delete form on GET
+exports.author_delete_get = function (req, res) {
+	res.send('TODO: Author delete GET');
+};
+
+// Handle Author delete on POST
+exports.author_delete_post = function (req, res) {
+	res.send('TODO: Author delete POST');
+};
+
+// Display Author update form on GET
+exports.author_update_get = function (req, res) {
+	res.send('TODO: Author update GET');
+};
+
+// Handle Author update on POST
+exports.author_update_post = function (req, res) {
+	res.send('TODO: Author update POST');
+};
+
